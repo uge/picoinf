d175a24c27111672128c0a7c208fe38f7600d43f
Douglas Malnati <douglas.malnati@gmail.com>
2024-02-12 16:56:21 -0500

    tusb_config change defines to be non-conditional

--- lib/tusb_config.core.h
+++ lib/tusb_config.core.h
@@ -21,11 +21,11 @@
 #define CFG_TUD_CDC  1
 
 // CDC FIFO size of TX and RX
-#define CFG_TUD_CDC_RX_BUFSIZE   (TUD_OPT_HIGH_SPEED ? 512 : 64)
-#define CFG_TUD_CDC_TX_BUFSIZE   (TUD_OPT_HIGH_SPEED ? 512 : 64)
+#define CFG_TUD_CDC_RX_BUFSIZE   64
+#define CFG_TUD_CDC_TX_BUFSIZE   64
 
-// CDC Endpoint transfer buffer size, more is faster
-#define CFG_TUD_CDC_EP_BUFSIZE   (TUD_OPT_HIGH_SPEED ? 512 : 64)
+// CDC Endpoint transfer buffer size
+#define CFG_TUD_CDC_EP_BUFSIZE   64
 
 
 /////////////////////////////////////////////////////////////////////
