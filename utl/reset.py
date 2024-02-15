72cdaadeebc9422b5e8220547f8a4a4c0fc36b99
Douglas Malnati <douglas.malnati@gmail.com>
2024-02-15 17:48:35 -0500

    can reset device from task

---
+++ utl/reset.py
@@ -0,0 +1,30 @@
+import os
+import sys
+
+
+def Cmd(cmd):
+    os.system(cmd)
+
+def Main():
+    tmpDir = os.environ.get("temp")
+    scriptFile = "%s\\jlink.jls" % (tmpDir)
+
+    fdScript = open(scriptFile, "w")
+    fdScript.truncate()
+    fdScript.write(
+"""si 1
+speed 4000
+device RP2040_M0_0
+connect
+reset
+go
+exit
+""")
+    fdScript.close()
+
+    Cmd("JLink.exe -CommandFile %s -NoGui 1 > nul 2>&1" % (scriptFile))
+
+
+Main()
+
+
