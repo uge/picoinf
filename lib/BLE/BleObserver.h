aac163ea4e86c58bfc00c4faea716bd78e9e7316
Douglas Malnati <douglas.malnati@gmail.com>
2024-02-25 23:11:52 -0500

    BleObserver scan with filtering

--- lib/BLE/BleObserver.h
+++ lib/BLE/BleObserver.h
@@ -24,7 +24,14 @@ class BleObserver
 {
     friend class Ble;
 
-private:
+public:
+
+    enum class AdType : uint8_t
+    {
+        ADV_IND  = 0,
+        SCAN_RSP = 4,
+    };
+
 
     struct AdReport
     {
@@ -35,6 +42,7 @@ private:
         }
 
         bd_addr_t address;
+        char      addrStr[6*3]; // 12:45:78:01:34:67\0
         uint8_t   addressType;
         uint8_t   eventType;
         int8_t    rssi;
@@ -42,6 +50,33 @@ private:
         uint8_t   data[BleAdvertisingDataFormatter::ADV_BYTES];
     };
 
+    struct Filter
+    {
+        struct Attr
+        {
+            enum class Type : uint8_t
+            {
+                AD_TYPE,
+                // MAC,
+                UUID16,
+                // MFR_DATA_SIZE,
+                DEVICE_NAME,
+            };
+
+            // attribute type
+            Type type;
+
+            // attribute value, depending on type
+            AdType eventType;
+            UUID uuid;
+            string deviceName;
+        };
+
+        // list of attributes to filter against
+        vector<Attr> attrList;
+        
+        bool filterDups = false;
+    };
 
 
 public:
@@ -50,9 +85,9 @@ public:
     // Public Interface
     /////////////////////////////////////////////////////////////////
 
-    static void Start()
+    static void Start(function<void(const AdReport &report)> cbFn, vector<Filter> filterList = {})
     {
-        if (started_) return;
+        Stop();
 
         if (ready_ == false)
         {
@@ -61,28 +96,36 @@ public:
             // knowing that detail.
 
             // We know that by the time Evm comes around, BLE is available
-            ted_.SetCallback(Start);
+            ted_.SetCallback([=]{
+                Start(cbFn, filterList);
+            }, "BleObserver::Start Timer");
             ted_.RegisterForTimedEvent(0);
         }
         else
         {
             started_ = true;
 
-            // scan_type – 0 = passive, 1 = active
+            // capture state
+            cbFn_ = cbFn;
+            filterList_ = filterList;
+
+            // Do some one-time adjustments to the filter
+            PreprocessFilterList(filterList_);
+
+            // Determine whether we can specify primary advertisements
+            // only (as an optimization), or if we need scan response
+            // also
+            uint8_t scanType = GetRequiredScanType(filterList_);
+
             // scan_interval – range 0x0004..0x4000, unit 0.625 ms
             // scan_window – range 0x0004..0x4000, unit 0.625 ms
             // scanning_filter_policy – 0 = all devices, 1 = all from whitelist
             // uint8_t scan_type, uint16_t scan_interval, uint16_t scan_window
 
             // Active scanning, 100% (scan interval = scan window)
-            gap_set_scan_parameters(1, 48, 48);
+            gap_set_scan_parameters(scanType, 48, 48);
 
             gap_start_scan();
-
-            ted_.SetCallback([]{
-                ClearCache();
-            });
-            ted_.RegisterForTimedEventInterval(CACHE_EXPIRY_SECS * 1000);
         }
     }
 
@@ -95,16 +138,66 @@ public:
 
         gap_stop_scan();
 
-        ClearCache();
+        Evm::ClearLowPriorityWorkByLabel(LOW_PRIO_WORK_LABEL);
     }
 
-    static void ClearCache()
+
+private:
+
+    /////////////////////////////////////////////////////////////////
+    // Report Handling
+    /////////////////////////////////////////////////////////////////
+
+    static void OnAdvertisingReportEvm()
     {
-        IrqLock lock;
-        seenSet_.clear();
-        seenAddrSet_.clear();
+        AdReport report;
+
+        while (pipe_.Get(report, 0))
+        {
+            cbFn_(report);
+        }
     }
 
+    static void OnAdvertisingReport(uint8_t *packet)
+    {
+        uint8_t type = hci_event_packet_get_type(packet);
+
+        if (type == GAP_EVENT_ADVERTISING_REPORT)
+        {
+            // Extract the report contents
+            AdReport report;
+
+            gap_event_advertising_report_get_address(packet, report.address);
+
+            report.eventType    = gap_event_advertising_report_get_advertising_event_type(packet);
+            report.addressType  = gap_event_advertising_report_get_address_type(packet);
+            report.rssi         = gap_event_advertising_report_get_rssi(packet);
+            report.len          = gap_event_advertising_report_get_data_length(packet);
+
+            memcpy(report.data, gap_event_advertising_report_get_data(packet), report.len);
+
+            // Do filtering
+            bool match = FilterMatch(report, filterList_);
+
+            if (match)
+            {
+                // finalize structure
+                memcpy(report.addrStr,bd_addr_to_str(report.address), sizeof(report.addrStr));
+
+                // pass upward if not already an event ascending
+                auto count = pipe_.Count();
+
+                if (pipe_.Put(report, 0))
+                {
+                    // send an event if there isn't one already pending
+                    if (count == 0)
+                    {
+                        Evm::QueueLowPriorityWork(LOW_PRIO_WORK_LABEL, OnAdvertisingReportEvm);
+                    }
+                }
+            }
+        }
+    }
 
 private:
 
@@ -121,149 +214,245 @@ private:
 
 
     /////////////////////////////////////////////////////////////////
-    // Report Handling
+    // Filter Related
     /////////////////////////////////////////////////////////////////
 
-    static void OnAdvertisingReportEvm()
+    static uint8_t GetRequiredScanType(const vector<Filter> &filterList)
     {
-        AdReport report;
+        // scan_type – 0 = passive, 1 = active
+        uint8_t retVal = 0;
 
-        while (pipe_.Get(report, 0))
+        // check the filter to see if we can avoid doing active scanning
+        // (ie avoid asking for a scan response).  If any specify scan response
+        // or any don't specify at all, must do active scan.
+        size_t countSpecified = 0;
+        for (const auto &filter : filterList_)
         {
-            // commit hash so ISR can pre-filter
-            uint32_t hash = HashReport(report);
-            {
-                IrqLock lock;
-                seenSet_.insert(hash);
-            }
-            Log("Hash: ", ToHex(hash), ", now ", seenSet_.size(), " seen");
+            bool didSpecifyType = false;
 
-            // check if this is changed data for a particular address.
-            // not functionality which is required, just investigating.
-            string addrStr = bd_addr_to_str(report.address);
+            for (const auto &attr : filter.attrList)
+            {
+                if (attr.type == Filter::Attr::Type::AD_TYPE)
+                {
+                    didSpecifyType = true;
 
-            if (seenAddrSet_.contains(addrStr) == false)
-            {
-                seenAddrSet_.insert(addrStr);
-                Log("New: ", seenAddrSet_.size(), " addresses so far");
-            }
-            else
-            {
-                Log("Mod: Address ", addrStr, " seen before, changed");
+                    if (attr.eventType == AdType::SCAN_RSP)
+                    {
+                        retVal = 1;
+                    }
+                }
             }
 
-            // print out report
-            string eventTypeStr = typeMap_.contains(report.eventType) ?
-                                  typeMap_.at(report.eventType)       :
-                                  to_string(report.eventType);
-
-            Log("Ad: addrType: ", report.addressType,
-                ", addr: ", addrStr,
-                ", rssi: ", report.rssi,
-                ", len: ", report.len,
-                " eventType: ", eventTypeStr);
-
-            // dump_advertisement_data(report.data, report.len);
-
-            LogNL();
+            if (didSpecifyType)
+            {
+                ++countSpecified;
+            }
         }
+
+        if (countSpecified != filterList.size())
+        {
+            retVal = 1;
+        }
+
+        return retVal;
     }
 
 
-    static void OnAdvertisingReport(uint8_t *packet)
+    /////////////////////////////////////////////////////////////////
+    // Filter Matching
+    /////////////////////////////////////////////////////////////////
+
+//                    EVM_TIMED_END -              OnAdvertisingReport:  17 ms,  17,635 us - 0:00:01.838024
+//              OnAdvertisingReport -                        Extracted:   0 ms,      22 us - 0:00:01.838046
+//                        Extracted -                         Filtered:   0 ms,     142 us - 0:00:01.838188
+//                         Filtered -              OnAdvertisingReport:   0 ms,     838 us - 0:00:01.839026
+//              OnAdvertisingReport -                        Extracted:   0 ms,      10 us - 0:00:01.839036
+//                        Extracted -                         Filtered:   0 ms,      84 us - 0:00:01.839120
+//                         Filtered -              OnAdvertisingReport:   4 ms,   4,778 us - 0:00:01.843898
+//              OnAdvertisingReport -                        Extracted:   0 ms,      17 us - 0:00:01.843915
+//                        Extracted -                         Filtered:   0 ms,      95 us - 0:00:01.844010
+//                         Filtered -              OnAdvertisingReport:   0 ms,     793 us - 0:00:01.844803
+//              OnAdvertisingReport -                        Extracted:   0 ms,      11 us - 0:00:01.844814
+//                        Extracted -                         Filtered:   0 ms,      96 us - 0:00:01.844910
+//                         Filtered -                              Put:   0 ms,      41 us - 0:00:01.844951
+//                              Put - BleObserver::OnAdvertisingReport:   0 ms,      15 us - 0:00:01.844966
+// BleObserver::OnAdvertisingReport -                           Queued:   0 ms,      51 us - 0:00:01.845017
+//                           Queued -          EVM_LOW_PRIO_WORK_START:   0 ms,     932 us - 0:00:01.845949
+//          EVM_LOW_PRIO_WORK_START -           OnAdvertisingReportEvm:   0 ms,      15 us - 0:00:01.845964
+//           OnAdvertisingReportEvm -                           Popped:   0 ms,      24 us - 0:00:01.845988
+//                           Popped -                              App:   0 ms,       8 us - 0:00:01.845996
+
+
+    static void PreprocessFilterList(vector<Filter> &filterList)
     {
-        static Pin p(18);
-
-        uint8_t type = hci_event_packet_get_type(packet);
-
-        if (type == GAP_EVENT_ADVERTISING_REPORT)
+        for (auto &filter : filterList_)
         {
-            AdReport report;
-
-            gap_event_advertising_report_get_address(packet, report.address);
-
-            report.eventType    = gap_event_advertising_report_get_advertising_event_type(packet);
-            report.addressType  = gap_event_advertising_report_get_address_type(packet);
-            report.rssi         = gap_event_advertising_report_get_rssi(packet);
-            report.len          = gap_event_advertising_report_get_data_length(packet);
-
-            memcpy(report.data, gap_event_advertising_report_get_data(packet), report.len);
-
-            bool ourGuy = (strcmp("28:CD:C1:08:7D:74", bd_addr_to_str(report.address)) == 0) &&
-                          (report.eventType == 0 || report.eventType == 0b0010);
-
-            uint32_t hash = HashReport(report);
-
-            if (seenSet_.contains(hash) == false && ourGuy)
+            for (auto &attr : filter.attrList)
             {
-                p.DigitalToggle();
-            }
-
-            if (ourGuy)
-            {
-                static uint64_t timeLast = 0;
-
-                uint64_t timeNow = PAL.Micros();
-
-                if (timeLast != 0)
+                if (attr.type == Filter::Attr::Type::UUID16)
                 {
-                    uint64_t timeDiff = timeNow - timeLast;
-                    Log(Commas(timeDiff / 1000), " ms since last");
+                    attr.uuid.ReverseBytes();
                 }
-
-                // p.DigitalToggle();
-                Log("[", TimestampFromUs(PAL.Micros()), "]: Our guy, hash (", ToHex(hash), ") seen? ", seenSet_.contains(hash) ? "yes" : "no");
-                LogBlob(report.data, report.len);
-                LogNL();
-
-                timeLast = timeNow;
             }
+        }
+    }
 
-            if (seenSet_.contains(hash) == false && ourGuy)
+    static auto GetDataByType(const AdReport &report, uint8_t type)
+    {
+        const uint8_t *retVal = nullptr;
+        uint8_t len = 0;
+
+        ad_context_t context;
+        for (ad_iterator_init(&context, report.len, report.data);
+             ad_iterator_has_more(&context);
+             ad_iterator_next(&context))
+        {
+            uint8_t dataType = ad_iterator_get_data_type(&context);
+
+            if (dataType == type)
             {
-                auto count = pipe_.Count();
+                retVal = ad_iterator_get_data(&context);
+                len    = ad_iterator_get_data_len(&context);
 
-                if (pipe_.Put(report, 0))
+                break;
+            }
+        }
+
+        return pair{ retVal, len };
+    }
+
+    static bool MatchUuid16(const AdReport &report, const UUID &uuid)
+    {
+        static const uint8_t BT_DATA_UUID16_ALL   = 0x03;
+        static const uint8_t BT_DATA_UUID16_SOME  = 0x02;
+
+        bool retVal = false;
+
+        vector<uint8_t> typeList = { BT_DATA_UUID16_ALL, BT_DATA_UUID16_SOME };
+
+        for (auto type : typeList)
+        {
+            auto [data, len] = GetDataByType(report, type);
+
+            if (data && len >= 2)
+            {
+                const uint8_t *uuidBuf = uuid.GetBytes();
+
+                for (int i = 0; i < len; i += 2)
                 {
-                    // send an event if there isn't one already pending
-                    if (count == 0)
+                    const uint8_t *p = &data[i];
+
+                    retVal = memcmp(p, uuidBuf, 2) == 0;
+
+                    if (retVal)
                     {
-                        Evm::QueueWork("BleObserver::OnAdvertisingReport", OnAdvertisingReportEvm);
+                        break;
                     }
                 }
             }
         }
+
+        return retVal;
     }
 
-
-private:
-
-    static uint32_t HashReport(const AdReport &report)
+    static bool MatchDeviceName(const AdReport &report, const string &deviceName)
     {
-        XXHash32 hashBuilder(0);
+        static const uint8_t TYPE = 0x09;
 
-        hashBuilder.add(report.address, sizeof(report.address));
-        hashBuilder.add(&report.eventType, sizeof(report.eventType));
-        hashBuilder.add(report.data, report.len);
+        bool retVal = false;
 
-        return hashBuilder.hash();
+        auto [data, len] = GetDataByType(report, TYPE);
+
+        if (data && len == (uint8_t)deviceName.length())
+        {
+            retVal = memcmp(data, deviceName.c_str(), len) == 0;
+        }
+
+        return retVal;
     }
+
+    static bool FilterMatch(const AdReport &report, const Filter &filter)
+    {
+        size_t countMatch = 0;
+
+        for (const auto &attr : filter.attrList)
+        {
+            if (attr.type == Filter::Attr::Type::AD_TYPE)
+            {
+                if ((uint8_t)attr.eventType == report.eventType)
+                {
+                    ++countMatch;
+                }
+            }
+            else if (attr.type == Filter::Attr::Type::UUID16)
+            {
+                if (MatchUuid16(report, attr.uuid))
+                {
+                    ++countMatch;
+                }
+            }
+            else if (attr.type == Filter::Attr::Type::DEVICE_NAME)
+            {
+                if (MatchDeviceName(report, attr.deviceName))
+                {
+                    ++countMatch;
+                }
+            }
+        }
+
+        return countMatch == filter.attrList.size();
+    }
+
+    static bool FilterMatch(const AdReport &report, const vector<Filter> &filterList)
+    {
+        bool retVal = false;
+
+        if (filterList.size())
+        {
+            for (const auto &filter : filterList)
+            {
+                if (FilterMatch(report, filter))
+                {
+                    retVal = true;
+
+                    break;
+                }
+            }
+        }
+        else
+        {
+            retVal = true;
+        }
+
+        return retVal;
+    }
+
+
+    /////////////////////////////////////////////////////////////////
+    // Misc
+    /////////////////////////////////////////////////////////////////
 
     static void SetupShell()
     {
-        Shell::AddCommand("ble.scan.cache.report", [](vector<string> argList){
-            IrqLock lock;
-            Log("Seen Set: ", seenSet_.size());
-            Log("Addr Set: ", seenAddrSet_.size());
-        }, { .argCount = 0, .help = "Report on count of entries in cache" });
+        Shell::AddCommand("ble.scan.start", [](vector<string> argList){
+            Start([](const AdReport &report){
+                Log("Ad: addrType: ", report.addressType,
+                    ", addr: ", report.addrStr,
+                    ", rssi: ", report.rssi,
+                    ", len: ", report.len,
+                    " eventType: ", report.eventType);
+            });
+        }, { .argCount = 0, .help = "Scan start and report seen" });
 
-        Shell::AddCommand("ble.scan.cache.clear", [](vector<string> argList){
-            ClearCache();
-        }, { .argCount = 0, .help = "Clear entries in cache" });
+        Shell::AddCommand("ble.scan.stop", [](vector<string> argList){
+            Stop();
+        }, { .argCount = 0, .help = "Scan stop" });
     }
 
 
 private:
+
+    constexpr static const char *LOW_PRIO_WORK_LABEL = "BleObserver Low Priority Work";
 
     inline static unordered_map<uint8_t, string> typeMap_ = {
         { 0b0000, "ADV_IND"                       },
@@ -280,11 +469,13 @@ private:
     static const uint8_t MAX_EVENTS = 8;
     inline static KMessagePipe<AdReport, MAX_EVENTS> pipe_;
 
-    // must not allow unbounded growth, reset these periodically
-    inline static const uint64_t CACHE_EXPIRY_SECS = 5 * 60;   // 5 min
+    // callbacks
+    inline static function<void(const AdReport &report)> cbFn_ = [](const AdReport &){};
+
+    // filter
+    inline static vector<Filter> filterList_;
+
     inline static TimedEventHandlerDelegate ted_;
-    inline static unordered_set<uint32_t> seenSet_;
-    inline static unordered_set<string> seenAddrSet_;
 
     inline static bool started_ = false;
 };
