4c640d6fb5341e4f5761564e77a84d962b3d0cc0
Douglas Malnati <douglas.malnati@gmail.com>
2024-03-16 19:57:03 -0400

    I2C ported

--- lib/App/Peripheral/I2C.cpp
+++ lib/App/Peripheral/I2C.cpp
@@ -1,21 +1,11 @@
 #include "I2C.h"
-
-#include <zephyr/net/net_ip.h>
-#include <zephyr/device.h>
-#include <zephyr/drivers/i2c.h>
-
 #include "Log.h"
+#include "Timeline.h"
 #include "Utl.h"
 
+#include "hardware/i2c.h"
 
-// https://docs.zephyrproject.org/latest/build/dts/api/bindings/i2c/nordic,nrf-twi.html
-// https://docs.zephyrproject.org/latest/hardware/peripherals/i2c.html
-// https://docs.zephyrproject.org/2.7.0/reference/devicetree/bindings/i2c/nordic%2Cnrf-twi.html
-// https://www.beyondlogic.org/devicetree-overlays-on-zephyr-rtos-adding-i2c-or-spi/
-
-// https://infocenter.nordicsemi.com/index.jsp?topic=%2Fps_nrf52840%2Ftwi.html
-// https://infocenter.nordicsemi.com/index.jsp?topic=%2Fps_nrf52840%2Ftwi.html&cp=5_0_0_5_28_7_0&anchor=register.TASKS_STARTRX
-
+#include "StrictMode.h"
 
 
 // the first param of each is the dev ptr.
@@ -45,26 +35,13 @@
 //
 
 
-
-I2C::I2C()
-{
-    dev_ = DEVICE_DT_GET(DT_NODELABEL(i2c0));
-}
-
-void I2C::RecoverBus()
-{
-    i2c_recover_bus(dev_);
-}
-
 bool I2C::CheckAddr(uint8_t addr)
 {
     uint8_t byte;
 
-    RecoverBus();
-    int ret = i2c_read(dev_, &byte, 1, addr);
-    SetOk(ret);
+    int ret = i2c_read_blocking(i2c0, addr, &byte, 1, false);
 
-    return !ret;
+    return ret >= 0;
 }
 
 vector<uint8_t> I2C::Scan()
@@ -86,9 +63,8 @@ uint8_t I2C::ReadReg8(uint8_t addr, uint8_t reg)
 {
     uint8_t retVal;
     
-    RecoverBus();
-    int ret = i2c_write_read(dev_, addr, &reg, 1, &retVal, 1);
-    SetOk(ret);
+    i2c_write_blocking(i2c0, addr, &reg, 1, true);
+    i2c_read_blocking(i2c0, addr, &retVal, 1, false);
 
     return retVal;
 }
@@ -98,11 +74,10 @@ uint16_t I2C::ReadReg16(uint8_t addr, uint8_t reg)
     uint16_t retVal;
 
     // I2C is MSB, so create our own MSB buffer
-    uint16_t buf;
+    uint16_t buf = 0;
 
-    RecoverBus();
-    int ret = i2c_write_read(dev_, addr, &reg, 1, (uint8_t *)&buf, sizeof(buf));
-    SetOk(ret);
+    i2c_write_blocking(i2c0, addr, &reg, 1, true);
+    i2c_read_blocking(i2c0, addr, (uint8_t *)&buf, 2, false);
 
     // put in host order
     retVal = ntohs(buf);
@@ -112,9 +87,12 @@ uint16_t I2C::ReadReg16(uint8_t addr, uint8_t reg)
 
 void I2C::WriteReg8(uint8_t addr, uint8_t reg, uint8_t val)
 {
-    RecoverBus();
-    int ret = i2c_reg_write_byte(dev_, addr, reg, val);
-    SetOk(ret);
+    uint8_t buf[] = {
+        reg,
+        val,
+    };
+
+    i2c_write_blocking(i2c0, addr, (uint8_t *)&buf, sizeof(buf), false);
 }
 
 void I2C::WriteReg16(uint8_t addr, uint8_t reg, uint16_t val)
@@ -124,18 +102,16 @@ void I2C::WriteReg16(uint8_t addr, uint8_t reg, uint16_t val)
     // The first byte sent (after the addr) is the register to target,
     // then the actual bytes for that register.
     //
-    // We will pack our own 3-byte buffer.
+    // We will pack our own buffer.
     // I2C is MSB.
 
-    uint8_t buf[3] = {
+    uint8_t buf[] = {
         reg,
         (uint8_t)((val >> 8) & 0xFF),
         (uint8_t)(val & 0xFF),
     };
 
-    RecoverBus();
-    int ret = i2c_write(dev_, buf, 3, addr);
-    SetOk(ret);
+    i2c_write_blocking(i2c0, addr, (uint8_t *)&buf, sizeof(buf), false);
 }
 
 // No endianness conversions done here, caller has to swap around bytes
@@ -149,35 +125,15 @@ void I2C::WriteDirect(uint8_t addr, uint8_t reg, uint8_t *buf, uint8_t bufSize)
     //
     // We will pack our own bufSize+1-byte buffer.
     // I2C is MSB, but we don't care, transfer as-is.
-    uint8_t localBuf[bufSize + 1];
+    uint8_t bufLocal[bufSize + 1];
 
     // drop in the target register
-    localBuf[0] = reg;
+    bufLocal[0] = reg;
 
     // copy in all the data that was given
-    memcpy(&localBuf[1], buf, bufSize);
+    memcpy(&bufLocal[1], buf, bufSize);
 
-    RecoverBus();
-    int ret = i2c_write(dev_, localBuf, bufSize + 1, addr);
-    SetOk(ret);
-}
-
-bool I2C::Ok()
-{
-    return ok_;
-}
-
-void I2C::ResetOk()
-{
-    ok_ = true;
-}
-
-void I2C::SetOk(int ret)
-{
-    if (ret < 0)
-    {
-        ok_ = ret && ok_;
-    }
+    i2c_write_blocking(i2c0, addr, (uint8_t *)&bufLocal, sizeof(bufLocal), false);
 }
 
 
@@ -185,9 +141,20 @@ void I2C::SetOk(int ret)
 // Initilization
 ////////////////////////////////////////////////////////////////////////////////
 
-int I2CSetupShell()
+void I2C::Init()
 {
-    Timeline::Global().Event("I2CSetupShell");
+    Timeline::Global().Event("I2C::Init");
+
+    i2c_init(i2c0, 1000 * 1000);
+    gpio_set_function(4, GPIO_FUNC_I2C);
+    gpio_set_function(5, GPIO_FUNC_I2C);
+    gpio_pull_up(4);
+    gpio_pull_up(5);
+}
+
+void I2C::SetupShell()
+{
+    Timeline::Global().Event("I2C::SetupShell");
 
     static I2C i2c;
 
@@ -200,82 +167,66 @@ int I2CSetupShell()
             LogNNL(" ", Format::ToHex(addr), "(", addr, ")");
         }
         LogNL();
-
-        i2c.ResetOk();
     }, { .argCount = 0, .help = "I2C scan all addresses"});
 
     Shell::AddCommand("i2c.addr", [&](vector<string> argList){
         string hexAddr = argList[0];
-        uint8_t addr = Format::FromHex(hexAddr);
+        uint8_t addr = (uint8_t)Format::FromHex(hexAddr);
 
         Log("Addr ", hexAddr, "(", addr, "): ", i2c.CheckAddr(addr) ? "alive" : "not alive");
-
-        i2c.ResetOk();
     }, { .argCount = 1, .help = "I2C check hexAddr alive"});
 
     Shell::AddCommand("i2c.read8", [&](vector<string> argList){
         string hexAddr = argList[0];
-        uint8_t addr = Format::FromHex(hexAddr);
+        uint8_t addr = (uint8_t)Format::FromHex(hexAddr);
         string hexReg = argList[1];
-        uint8_t reg = Format::FromHex(hexReg);
+        uint8_t reg = (uint8_t)Format::FromHex(hexReg);
 
         uint8_t val = i2c.ReadReg8(addr, reg);
 
         Log("Reading ", hexAddr, "(", addr, ") ", hexReg, "(", reg, ")");
-        Log("Val (", i2c.Ok() ? "ok" : "bad", "): ", Format::ToHex(val), "(", val, ")(", Format::ToBin(val), ")");
-
-        i2c.ResetOk();
+        Log(Format::ToHex(val), "(", val, ")(", Format::ToBin(val), ")");
     }, { .argCount = 2, .help = "I2C read <hexAddr> <hexReg>"});
 
     Shell::AddCommand("i2c.read16", [&](vector<string> argList){
         string hexAddr = argList[0];
-        uint8_t addr = Format::FromHex(hexAddr);
+        uint8_t addr = (uint8_t)Format::FromHex(hexAddr);
         string hexReg = argList[1];
-        uint8_t reg = Format::FromHex(hexReg);
+        uint8_t reg = (uint8_t)Format::FromHex(hexReg);
 
         uint16_t val = i2c.ReadReg16(addr, reg);
 
         Log("Reading ", hexAddr, "(", addr, ") ", hexReg, "(", reg, ")");
-        Log("Val (", i2c.Ok() ? "ok" : "bad", "): ", Format::ToHex(val), "(", val, ")(", Format::ToBin(val), ")");
-
-        i2c.ResetOk();
+        Log(Format::ToHex(val), "(", val, ")(", Format::ToBin(val), ")");
     }, { .argCount = 2, .help = "I2C read <hexAddr> <hexReg>"});
 
     Shell::AddCommand("i2c.write8", [&](vector<string> argList){
         string hexAddr = argList[0];
-        uint8_t addr = Format::FromHex(hexAddr);
+        uint8_t addr = (uint8_t)Format::FromHex(hexAddr);
         string hexReg = argList[1];
-        uint8_t reg = Format::FromHex(hexReg);
+        uint8_t reg = (uint8_t)Format::FromHex(hexReg);
         string hexVal = argList[2];
-        uint8_t val = Format::FromHex(hexVal);
+        uint8_t val = (uint8_t)Format::FromHex(hexVal);
 
         i2c.WriteReg8(addr, reg, val);
 
-        Log("Val (", i2c.Ok() ? "ok" : "bad", "): ", Format::ToHex(val), "(", val, ")(", Format::ToBin(val), ")");
-
-        i2c.ResetOk();
+        Log(Format::ToHex(val), "(", val, ")(", Format::ToBin(val), ")");
     }, { .argCount = 3, .help = "I2C write <hexAddr> <hexReg> <hexVal>"});
 
     Shell::AddCommand("i2c.write16", [&](vector<string> argList){
         string hexAddr = argList[0];
-        uint8_t addr = Format::FromHex(hexAddr);
+        uint8_t addr = (uint8_t)Format::FromHex(hexAddr);
         string hexReg = argList[1];
-        uint8_t reg = Format::FromHex(hexReg);
+        uint8_t reg = (uint8_t)Format::FromHex(hexReg);
         string hexVal = argList[2];
-        uint16_t val = Format::FromHex(hexVal);
+        uint16_t val = (uint8_t)Format::FromHex(hexVal);
 
         i2c.WriteReg16(addr, reg, val);
 
-        Log("Val (", i2c.Ok() ? "ok" : "bad", "): ", Format::ToHex(val), "(", val, ")(", Format::ToBin(val), ")");
-
-        i2c.ResetOk();
+        Log(Format::ToHex(val), "(", val, ")(", Format::ToBin(val), ")");
     }, { .argCount = 3, .help = "I2C write <hexAddr> <hexReg> <hexVal>"});
-
-    return 1;
 }
 
-#include <zephyr/init.h>
-SYS_INIT(I2CSetupShell, APPLICATION, 80);
 
 
 
--- lib/App/Peripheral/I2C.h
+++ lib/App/Peripheral/I2C.h
@@ -4,14 +4,10 @@
 
 #include <vector>
 
-#include <zephyr/device.h>
-
 
 class I2C
 {
 public:
-    I2C();
-    void RecoverBus();
     bool CheckAddr(uint8_t addr);
     std::vector<uint8_t> Scan();
     uint8_t ReadReg8(uint8_t addr, uint8_t reg);
@@ -19,15 +15,11 @@ public:
     void WriteReg8(uint8_t addr, uint8_t reg, uint8_t val);
     void WriteReg16(uint8_t addr, uint8_t reg, uint16_t val);
     void WriteDirect(uint8_t addr, uint8_t reg, uint8_t *buf, uint8_t bufSize);
-    bool Ok();
-    void ResetOk();
+
+    static void Init();
+    static void SetupShell();
 
 private:
-
-    void SetOk(int ret);
-
-    const device *dev_ = nullptr;
-    bool ok_ = true;
 };
 
 
