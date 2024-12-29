2224c49a001e09f14dd8a41b0ce91395c61c7950
dmalnati <douglas.malnati@gmail.com>
2024-12-29 09:50:55 -0500

    integrate JerryScript.
    starting size
    ROM:   436,124 / 2,097,152 = 21 % Used | 1,661,028 = 79 % Remaining
    RAM:    58,168 /   270,336 = 22 % Used |   212,168 = 78 % Remaining
    
    after first use of javascript
    ROM:   984,908 / 2,097,152 = 47 % Used | 1,112,244 = 53 % Remaining
    RAM:    65,084 /   270,336 = 25 % Used |   205,252 = 75 % Remaining

---
+++ ext/jerryscript.cmake
@@ -0,0 +1,79 @@
+#####################################################################
+# Build Environment Detection
+#
+# I want to be able to detect building against windows vs <else>,
+# where <else> is the RPi-Pico SDK.
+#
+#####################################################################
+
+set(IS_SDK_BUILD 1)
+if (WIN32)
+    set(IS_SDK_BUILD 0)
+endif()
+
+message(" ==== WIN32: ${WIN32} ===== ")
+message(" ==== IS_SDK_BUILD: ${IS_SDK_BUILD} ===== ")
+
+
+#####################################################################
+# Library Features
+#####################################################################
+
+if (IS_SDK_BUILD)
+    set(JERRY_PORT OFF) # Should the default/native port be implemented? No, I will implement my own
+
+    set(JERRY_CMDLINE OFF)  # a necessary requirement for JERRY_PORT to be OFF
+
+    set(ENABLE_LTO OFF) # doesn't work on sdk
+endif()
+
+
+#####################################################################
+# Engine Features
+#####################################################################
+
+# Logging and error diagnosis
+set(JERRY_LINE_INFO ON)
+set(JERRY_ERROR_MESSAGES ON)
+set(JERRY_LOGGING ON)
+
+# CPU time limiting
+set(JERRY_VM_HALT ON)
+set(JERRY_VM_THROW ON)
+
+# Memory limiting
+set(JERRY_GLOBAL_HEAP_SIZE 4)   # in KB
+set(JERRY_STACK_LIMIT 8)   # in KB
+
+# Memory limiting -- maybe.
+# Need to experiment and see effects (initially went up).
+set(JERRY_LCACHE OFF)
+set(JERRY_PROPERTY_HASHMAP OFF)
+
+# Investigating memory usage
+set(JERRY_MEM_STATS ON)
+
+
+#####################################################################
+# Language Features
+#####################################################################
+
+
+
+
+
+#####################################################################
+# Include
+#####################################################################
+
+# tons of compile warnings about printf formatting flags for some
+# reason that don't show up under windows.
+# suppress them, unfortunately.
+set(EXTERNAL_COMPILE_FLAGS
+    -Wno-format
+    -Wno-error=format
+    -Wno-format-nonliteral
+    -Wno-format-security
+)
+
+add_subdirectory(${CMAKE_CURRENT_LIST_DIR}/jerryscript)
