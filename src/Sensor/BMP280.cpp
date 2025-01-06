f21a6bb12ccdca8639052d99208fcdb244f57cdc
dmalnati <douglas.malnati@gmail.com>
2025-01-06 12:57:17 -0500

    BMP280 move implementation to cpp

--- src/Sensor/BMP280.cpp
+++ src/Sensor/BMP280.cpp
@@ -10,6 +10,56 @@ using namespace std;
 #include "StrictMode.h"
 
 
+// Address is either 0x76 or 0x77
+BMP280::BMP280(uint8_t addr, I2C::Instance instance)
+: tw_(addr, instance)
+, bmp280_(&tw_)
+{
+    // startup sequence taken from example ino.
+    // "Default settings from datasheet."
+    bmp280_.begin();
+    bmp280_.setSampling(Adafruit_BMP280::MODE_NORMAL,     /* Operating Mode. */
+                        Adafruit_BMP280::SAMPLING_X2,     /* Temp. oversampling */
+                        Adafruit_BMP280::SAMPLING_X16,    /* Pressure oversampling */
+                        Adafruit_BMP280::FILTER_X16,      /* Filtering. */
+                        Adafruit_BMP280::STANDBY_MS_500); /* Standby time. */
+}
+
+double BMP280::GetTemperatureCelsius()
+{
+    // sensor returns celsius
+    return bmp280_.readTemperature();
+}
+
+double BMP280::GetTemperatureFahrenheit()
+{
+    return (GetTemperatureCelsius() * (9.0 / 5.0)) + 32;
+}
+
+double BMP280::GetPressurehPa()
+{
+    // sensor returns Pa
+    return bmp280_.readPressure() / 100;
+}
+
+double BMP280::GetPressureBar()
+{
+    return GetPressurehPa() * 1'000;
+}
+
+double BMP280::GetAltitudeMeters()
+{
+    // sensor returns meters
+    // calibrated to nominal pressure at sea level (hPa)
+    return bmp280_.readAltitude(1013.25);
+}
+
+double BMP280::GetAltitudeFeet()
+{
+    return GetAltitudeMeters() * 3.28084;
+}
+
+
 void BMP280::SetupShell()
 {
     Timeline::Global().Event("BMP280::SetupShell");
