2bc2bb44fb317ab5ed0c33a92b5ad9818ff4f79d
dmalnati <douglas.malnati@gmail.com>
2025-01-22 19:03:33 -0500

    TimerSequence add StartAt

--- src/App/Evm/TimerSequence.cpp
+++ src/App/Evm/TimerSequence.cpp
@@ -13,60 +13,86 @@ TimerSequence::TimerSequence(const char *title)
     Reset();
 }
 
+void TimerSequence::Reset()
+{
+    timer_.Cancel();
+    
+    firstStep_ = Step{};
+    stepListIdx_ = 0;
+    stepList_.clear();
+    stepList_.shrink_to_fit();
+}
+
 TimerSequence &TimerSequence::Add(function<void()> fn)
 {
-    Step step = { fn, true, 0, };
+    Step step = { fn, ScheduleNextType::ScheduleIn };
 
     stepList_.push_back(step);
 
     return *this;
 }
 
-TimerSequence &TimerSequence::StepFromPause()
+TimerSequence &TimerSequence::DelayUs(uint64_t durationUs)
 {
-    SetLatestStepParameters(false, 0);
+    // update the last element in the sequence to call next after a delay
+    Step &step = stepList_.size() == 0 ? firstStep_ : stepList_.back();
+
+    step.scheduleNextType = ScheduleNextType::ScheduleIn;
+    step.scheduleNext     = true;
+    step.scheduleNextInUs = durationUs;
+
+    // add an additional element of nothing to represent that delay
+    Add([]{});
 
     return *this;
 }
 
-TimerSequence &TimerSequence::StepFromInUs(uint64_t durationUs)
+TimerSequence &TimerSequence::DelayMs(uint64_t durationMs)
 {
-    SetLatestStepParameters(true, durationUs);
+    DelayUs(durationMs * 1'000);
 
     return *this;
 }
 
-TimerSequence &TimerSequence::StepFromInMs(uint64_t durationMs)
+TimerSequence &TimerSequence::StartAtUs(uint64_t timeAtUs)
 {
-    SetLatestStepParameters(true, durationMs * 1'000);
+    if (stepList_.size() >= 1)
+    {
+        Step &step = stepList_.size() == 0 ? firstStep_ : stepList_[stepList_.size() - 2];
+
+        step.scheduleNextType = ScheduleNextType::ScheduleAt;
+        step.scheduleNext     = true;
+        step.scheduleNextAtUs = timeAtUs;
+    }
 
     return *this;
 }
+
+TimerSequence &TimerSequence::StartAtUs(function<uint64_t()> fn)
+{
+    if (stepList_.size() >= 1)
+    {
+        Step &step = stepList_.size() == 0 ? firstStep_ : stepList_[stepList_.size() - 2];
+
+        step.scheduleNextType = ScheduleNextType::ScheduleAtUsByFn;
+        step.scheduleNext     = true;
+        step.scheduleNextAtFn = fn;
+    }
+
+    return *this;
+}
+
+TimerSequence &TimerSequence::StartAtMs(uint64_t timeAtMs)
+{
+    return StartAtUs(timeAtMs * 1'000);
+}
+
 
 void TimerSequence::Start()
 {
-    timer_.TimeoutInMs(0);
+    ScheduleNextFromStep(firstStep_);
 }
 
-void TimerSequence::Reset()
-{
-    timer_.Cancel();
-    stepListIdx_ = 0;
-    stepList_.clear();
-    stepList_.shrink_to_fit();
-}
-
-
-void TimerSequence::SetLatestStepParameters(bool autoStep, uint64_t durationUs)
-{
-    if (stepList_.size())
-    {
-        Step &step = stepList_.back();
-
-        step.scheduleNext     = autoStep;
-        step.scheduleNextInUs = durationUs;
-    }
-}
 
 void TimerSequence::OnTimeout()
 {
@@ -76,17 +102,28 @@ void TimerSequence::OnTimeout()
 
         step.fn();
 
-        if (step.scheduleNext)
-        {
-            timer_.TimeoutInUs(step.scheduleNextInUs);
-        }
-
         ++stepListIdx_;
-    }
-    else
-    {
-        timer_.Cancel();
+
+        ScheduleNextFromStep(step);
     }
 }
 
 
+void TimerSequence::ScheduleNextFromStep(const Step &step)
+{
+    if (step.scheduleNext && stepListIdx_ < stepList_.size())
+    {
+        if (step.scheduleNextType == ScheduleNextType::ScheduleIn)
+        {
+            timer_.TimeoutInUs(step.scheduleNextInUs);
+        }
+        else if (step.scheduleNextType == ScheduleNextType::ScheduleAt)
+        {
+            timer_.TimeoutAtUs(step.scheduleNextAtUs);
+        }
+        else if (step.scheduleNextType == ScheduleNextType::ScheduleAtUsByFn)
+        {
+            timer_.TimeoutAtUs(step.scheduleNextAtFn());
+        }
+    }
+}
--- src/App/Evm/TimerSequence.h
+++ src/App/Evm/TimerSequence.h
@@ -10,33 +10,64 @@ class TimerSequence
 {
 public:
 
+    // constructor
     TimerSequence(const char *title = "TimerSequence");
-    
-    TimerSequence &Add(std::function<void()> fn);
-    TimerSequence &StepFromPause();
-    TimerSequence &StepFromInUs(uint64_t durationUs);
-    TimerSequence &StepFromInMs(uint64_t durationMs);
 
-    void Start();
+    // restore object to post-constructor state
     void Reset();
+
+    
+    // add an event which will be triggered asynchronously after the prior event
+    TimerSequence &Add(std::function<void()> fn);
+    
+    // add a delay after the most recently added event
+    TimerSequence &DelayUs(uint64_t durationUs);
+    TimerSequence &DelayMs(uint64_t durationMs);
+
+    // change the start time of the most-recently-added event
+    TimerSequence &StartAtUs(uint64_t timeAtUs);
+    TimerSequence &StartAtUs(std::function<uint64_t()> fn);
+    TimerSequence &StartAtMs(uint64_t timeAtMs);
+
+
+    // initiate sequence
+    void Start();
 
 
 private:
 
-    void SetLatestStepParameters(bool autoStep, uint64_t durationUs);
     void OnTimeout();
+
+    struct Step;
+    void ScheduleNextFromStep(const Step &step);
 
 
 private:
 
     Timer timer_;
 
+    enum class ScheduleNextType : uint8_t
+    {
+        ScheduleIn,
+        ScheduleAt,
+        ScheduleAtUsByFn,
+    };
+
     struct Step
     {
         std::function<void()> fn;
-        bool                  scheduleNext;
-        uint64_t              scheduleNextInUs;
+
+        ScheduleNextType scheduleNextType;
+
+        bool scheduleNext = true;
+
+        uint64_t scheduleNextInUs = 0;
+        uint64_t scheduleNextAtUs;
+
+        std::function<uint64_t()> scheduleNextAtFn;
     };
+
+    Step firstStep_;
 
     size_t            stepListIdx_ = 0;
     std::vector<Step> stepList_;
